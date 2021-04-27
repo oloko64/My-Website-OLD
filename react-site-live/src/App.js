ca2dd2e934649165d94015c740f8156e8b15cfd5
OLoko64 <naldoepingo1@gmail.com>
2021-04-27 14:17:39 -0300

    typos and fixes

--- react-site-live/src/App.js
+++ react-site-live/src/App.js
@@ -1,7 +1,7 @@
 import './css/defaults.css';
 import Home from './Home';
 import About from './About';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Portifolio from './Portifolio';
 import Curiosidades from './Curiosidades'
 import Certificados from './Certificados'
@@ -10,14 +10,17 @@ import Recomendados from './Recomendados'
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/" exact component={Home}></Route>
-      <Route path="/sobre" component={About}></Route>
-      <Route path="/portifolio" component={Portifolio}></Route>
-      <Route path="/curiosidades" component={Curiosidades}></Route>
-      <Route path="/livros-recomendados" component={Recomendados}></Route>
-      <Route path="/certificados" component={Certificados}></Route>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/sobre" component={About}></Route>
+          <Route path="/portifolio" component={Portifolio}></Route>
+          <Route path="/curiosidades" component={Curiosidades}></Route>
+          <Route path="/livros-recomendados" component={Recomendados}></Route>
+          <Route path="/certificados" component={Certificados}></Route>
+          <Route component={Home}></Route>
+        </Switch>
+      </div>
   );
 }
 
