247661c1939e0914d5a1cffcf2c743d89be29b33
OLoko64 <naldoepingo1@gmail.com>
2021-04-28 15:15:33 -0300

    navbar pop

--- react-site-live/src/components/navbar.jsx
+++ react-site-live/src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Header() {
             <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#161616cc'}}>
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">Home</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button style={{backgroundColor: '#3b3b3bcc'}} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
